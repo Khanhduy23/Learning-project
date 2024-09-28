f332ec4131cf44bd392a2a2cbad410151d7ce61c
Khanh Duy <lukiluna23803@gmail.com>
2024-09-29 05:05:57 +0700

    done ngu thoi

---
+++ assets/js/load-project-html.js
@@ -0,0 +1,148 @@
+const tutorialCards = [
+    { link: 'html-home.html', icon: 'fa-solid fa-home', title: 'HTML HOME' },
+    { link: 'html-introduction.html', icon: 'fa-solid fa-book-open', title: 'HTML Introduction' },
+    { link: 'html-editors.html', icon: 'fa-solid fa-edit', title: 'HTML Editors' },
+    { link: 'html-basic.html', icon: 'fa-solid fa-file-code', title: 'HTML Basic' },
+    { link: 'html-elements.html', icon: 'fa-solid fa-square', title: 'HTML Elements' },
+    { link: 'html-attributes.html', icon: 'fa-solid fa-tags', title: 'HTML Attributes' },
+    { link: 'html-headings.html', icon: 'fa-solid fa-heading', title: 'HTML Headings' },
+    { link: 'html-paragraphs.html', icon: 'fa-solid fa-paragraph', title: 'HTML Paragraphs' },
+    { link: 'html-styles.html', icon: 'fa-solid fa-paintbrush', title: 'HTML Styles' },
+    { link: 'html-formatting.html', icon: 'fa-solid fa-text-width', title: 'HTML Formatting' },
+    { link: 'html-quotations.html', icon: 'fa-solid fa-quote-left', title: 'HTML Quotations' },
+    { link: 'html-comments.html', icon: 'fa-solid fa-comment', title: 'HTML Comments' },
+    { link: 'html-colors.html', icon: 'fa-solid fa-palette', title: 'HTML Colors' },
+    { link: 'html-css.html', icon: 'fa-solid fa-css3-alt', title: 'HTML CSS' },
+    { link: 'html-links.html', icon: 'fa-solid fa-link', title: 'HTML Links' },
+    { link: 'html-images.html', icon: 'fa-solid fa-image', title: 'HTML Images' },
+    { link: 'html-favicon.html', icon: 'fa-solid fa-image', title: 'HTML Favicon' },
+    { link: 'html-page-title.html', icon: 'fa-solid fa-pencil-alt', title: 'HTML Page Title' },
+    { link: 'html-tables.html', icon: 'fa-solid fa-table', title: 'HTML Tables' },
+    { link: 'html-lists.html', icon: 'fa-solid fa-list', title: 'HTML Lists' },
+    { link: 'html-block-inline.html', icon: 'fa-solid fa-columns', title: 'HTML Block & Inline' },
+    { link: 'html-div.html', icon: 'fa-solid fa-box', title: 'HTML Div' },
+    { link: 'html-classes.html', icon: 'fa-solid fa-tags', title: 'HTML Classes' },
+    { link: 'html-id.html', icon: 'fa-solid fa-tag', title: 'HTML Id' },
+    { link: 'html-iframes.html', icon: 'fa-solid fa-window-restore', title: 'HTML Iframes' },
+    { link: 'html-javascript.html', icon: 'fa-solid fa-brands fa-js', title: 'HTML JavaScript' },
+    { link: 'html-file-paths.html', icon: 'fa-solid fa-folder-open', title: 'HTML File Paths' },
+    { link: 'html-head.html', icon: 'fa-solid fa-file', title: 'HTML Head' },
+    { link: 'html-layout.html', icon: 'fa-solid fa-th-large', title: 'HTML Layout' },
+    { link: 'html-responsive.html', icon: 'fa-solid fa-mobile-alt', title: 'HTML Responsive' },
+    { link: 'html-computercode.html', icon: 'fa-solid fa-desktop', title: 'HTML Computer Code' },
+    { link: 'html-semantics.html', icon: 'fa-solid fa-comment-alt', title: 'HTML Semantics' },
+    { link: 'html-style-guide.html', icon: 'fa-solid fa-book', title: 'HTML Style Guide' },
+    { link: 'html-entities.html', icon: 'fa-solid fa-database', title: 'HTML Entities' },
+    { link: 'html-symbols.html', icon: 'fa-solid fa-random', title: 'HTML Symbols' },
+    { link: 'html-emojis.html', icon: 'fa-solid fa-smile', title: 'HTML Emojis' },
+    { link: 'html-charsets.html', icon: 'fa-solid fa-font', title: 'HTML Charsets' },
+    { link: 'html-url-encode.html', icon: 'fa-solid fa-link', title: 'HTML URL Encode' },
+    { link: 'html-vs-xhtml.html', icon: 'fa-solid fa-signal', title: 'HTML vs. XHTML' },
+    { link: '#', icon: '', title: 'Trống' },
+    { link: '#', icon: '', title: 'Trống' },
+    { link: '#', icon: '', title: 'Trống' },
+];
+const formCards = [
+    { link: 'html-forms.html', icon: 'fa-solid fa-clipboard-list', title: 'HTML Forms' },
+    { link: 'html-form-attributes.html', icon: 'fa-solid fa-clipboard', title: 'HTML Form Attributes' },
+    { link: 'html-form-elements.html', icon: 'fa-solid fa-plus-square', title: 'HTML Form Elements' },
+    { link: 'html-input-types.html', icon: 'fa-solid fa-keyboard', title: 'HTML Input Types' },
+    { link: 'html-input-attributes.html', icon: 'fa-solid fa-key', title: 'HTML Input Attributes' },
+    { link: 'html-input-form-attributes.html', icon: 'fa-solid fa-plus-square', title: 'Input Form Attributes' },
+    { link: '#', icon: '', title: 'Trống' },
+];
+const grapCards = [
+    { link: 'html-canvas.html', icon: 'fa-solid fa-paint-brush', title: 'HTML Canvas' },
+    { link: 'html-svg.html', icon: 'fa-solid fa-vector-square', title: 'HTML SVG' },
+    { link: '#', icon: '', title: 'Trống' },
+    { link: '#', icon: '', title: 'Trống' },
+    { link: '#', icon: '', title: 'Trống' },
+    { link: '#', icon: '', title: 'Trống' },
+    { link: '#', icon: '', title: 'Trống' },
+
+];
+const mediaCards = [
+    { link: 'html-media.html', icon: 'fa-solid fa-video', title: 'HTML Media' },
+    { link: 'html-video.html', icon: 'fa-solid fa-video', title: 'HTML Video' },
+    { link: 'html-audio.html', icon: 'fa-solid fa-volume-up', title: 'HTML Audio' },
+    { link: 'html-plug-ins.html', icon: 'fa-solid fa-plug', title: 'HTML Plug-ins' },
+    { link: 'html-youtube.html', icon: 'fa-brands fa-youtube', title: 'HTML YouTube' },
+    { link: '#', icon: '', title: 'Trống' },
+    { link: '#', icon: '', title: 'Trống' }
+];
+const apisCards = [
+    { link: 'html-geolocation.html', icon: 'fa-solid fa-map-marked-alt', title: 'HTML Geolocation' },
+    { link: 'html-drag-drop.html', icon: 'fa-solid fa-arrows-alt', title: 'HTML Drag/Drop' },
+    { link: 'html-web-storage.html', icon: 'fa-solid fa-hdd', title: 'HTML Web Storage' },
+    { link: 'html-web-workers.html', icon: 'fa-solid fa-user-cog', title: 'HTML Web Workers' },
+    { link: 'html-sse.html', icon: 'fa-solid fa-user-friends', title: 'HTML SSE' },
+    { link: '#', icon: '', title: 'Trống' },
+    { link: '#', icon: '', title: 'Trống' }
+];
+const examCards = [
+    { link: 'html-examples.html', icon: 'fa-solid fa-code', title: 'HTML Examples' },
+    { link: 'html-editor.html', icon: 'fa-solid fa-code-branch', title: 'HTML Editor' },
+    { link: 'html-quiz.html', icon: 'fa-solid fa-question-circle', title: 'HTML Quiz' },
+    { link: 'html-exercises.html', icon: 'fa-solid fa-pencil-alt', title: 'HTML Exercises' },
+    { link: 'html-website.html', icon: 'fa-solid fa-globe', title: 'HTML Website' },
+    { link: 'html-interview-prep.html', icon: 'fa-solid fa-user-graduate', title: 'HTML Interview Prep' },
+    { link: 'html-bootcamp.html', icon: 'fa-solid fa-rocket', title: 'HTML Bootcamp' },
+    { link: 'html-certificate.html', icon: 'fa-solid fa-certificate', title: 'HTML Certificate' },
+    { link: 'html-summary.html', icon: 'fa-solid fa-book-open', title: 'HTML Summary' },
+    { link: 'html-accessibility.html', icon: 'fa-solid fa-universal-access', title: 'HTML Accessibility' },
+    { link: '#', icon: '', title: 'Trống' },
+    { link: '#', icon: '', title: 'Trống' },
+    { link: '#', icon: '', title: 'Trống' },
+    { link: '#', icon: '', title: 'Trống' }
+];
+function createCard(card) {
+    return `
+        <section class="card">
+            <a class="card__link" href="${card.link}">
+                <div class="card__content">
+                    <div class="card__icon">
+                        <i class="${card.icon}"></i>
+                    </div>
+                    <div class="card__title">${card.title}</div>
+                </div>
+            </a>
+        </section>
+    `;
+}
+
+function loadCards() {
+    const tutorialContainer = document.getElementById('tutorial-cards');
+    const formContainer = document.getElementById('form-cards');
+    const grapContainer = document.getElementById('grap-cards');
+    const mediaContainer = document.getElementById('media-cards');
+    const apisContainer = document.getElementById('apis-cards');
+    const examplesContainer = document.getElementById('examp-cards');
+
+    // Chèn dữ liệu cho phần HTML Tutorial
+    tutorialCards.forEach(card => {
+        tutorialContainer.innerHTML += createCard(card);
+    });
+
+    // Chèn dữ liệu cho phần HTML Form
+    formCards.forEach(card => {
+        formContainer.innerHTML += createCard(card);
+    });
+    // Chèn dữ liệu cho phần HTML Graphics
+    grapCards.forEach(card => {
+        grapContainer.innerHTML += createCard(card);
+    })
+    // Chèn dữ liệu cho phần HTML Media
+    mediaCards.forEach(card => {
+        mediaContainer.innerHTML += createCard(card);
+    });
+    // Chèn dữ liệu cho phần HTML APIs
+    apisCards.forEach(card => {
+        apisContainer.innerHTML += createCard(card)
+    });
+    examCards.forEach(card => {
+        examplesContainer.innerHTML += createCard(card);
+    });
+}
+
+// Gọi hàm để tải thẻ vào phần
+loadCards();
